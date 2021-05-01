390cca6af39a4f3294ee7d97deda10383eccca78
nhalflants <natacha_halflants@hotmail.com>
2021-05-01 16:39:15 +0200

    add hydrated bloc dependency

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -3,10 +3,12 @@ import 'dart:developer';
 
 import 'package:flutter/widgets.dart';
 import 'package:bloc/bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:weather/app/app.dart';
 import 'package:weather/app/app_bloc_observer.dart';
 import 'package:weather/location/location.dart';
 import 'package:weather_repository/weather_repository.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   Bloc.observer = AppBlocObserver();
@@ -18,6 +20,10 @@ void main() {
     FlutterError.onError = (FlutterErrorDetails details) {
       log(details.exceptionAsString(), stackTrace: details.stack);
     };
+
+    HydratedBloc.storage = await HydratedStorage.build(
+      storageDirectory: await getApplicationDocumentsDirectory(),
+    );
 
     // await Firebase.initializeApp();
     runApp(
